4e05c24a0b7ced411b2baefd65edd1d1086c0524
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2020-03-26 22:13:09 +0530

    Create Calculator.py
    This a functional calculator

---
+++ Calculator.py
@@ -0,0 +1,44 @@
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+def square(x, y):
+   return x * x
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.square")
+
+choice = input("Enter choice(1/2/3/4/5): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+   
+elif choice == '5' :
+    print(num1, "*", num1,"=" , multiply(num1,num1))
+   
+else:
+    print("Invalid input")                      #Author is ADITYA RAJ
