a2740a8c3a5421bd88edfda65ff0f7be2e4c5349
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-26 22:57:02 +0530

    datatype changed to integer type

--- Calculator.py
+++ Calculator.py
@@ -22,8 +22,8 @@ print("5.square")
 
 choice = input("Enter choice(1/2/3/4/5): ")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
